05b52697fb5cbf5a4c235aca61ae7d13f8dfc601
bpfree <brian.patrick.free@gmail.com>
2024-06-11 15:30:43 -0400

    aids to navigation

---
+++ code/42_aids_navigation.R
@@ -0,0 +1,158 @@
+##############################
+### 42. aids to navigation ###
+##############################
+
+# clear environment
+rm(list = ls())
+
+# calculate start time of code (determine how long it takes to complete all code)
+start <- Sys.time()
+
+#####################################
+#####################################
+
+# set parameters
+## designate region name
+region <- "gomex"
+
+## designate submodel
+submodel <- "constraints"
+
+## layer names
+data_name <- "aids_navigation"
+layer_name <- "aids to navigation"
+pattern <- "AtoN"
+
+## setback distance (in meters)
+setback <- 500
+
+## coordinate reference system
+### set the coordinate reference system that data should become (NAD83 / Conus Albers: https://epsg.io/5070)
+crs <- "EPSG:5070"
+
+## designate date
+date <- format(Sys.Date(), "%Y%m%d")
+
+#####################################
+#####################################
+
+# load packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(renv,
+               dplyr,
+               ggplot2,
+               janitor,
+               plyr,
+               purrr,
+               rmapshaper,
+               RSelenium,
+               sf,
+               sp,
+               stringr,
+               targets,
+               terra, # is replacing the raster package
+               tidyr)
+
+#####################################
+#####################################
+
+# set directories
+## define data directory (as this is an R Project, pathnames are simplified)
+### input directories
+#### aids to navigation sites
+data_dir <- "data/a_raw_data/AtoN/AtoN.gpkg"
+
+#### study area grid
+study_region_gpkg <- stringr::str_glue("data/b_intermediate_data/{region}_study_area.gpkg")
+
+### output directories
+#### submodel geopackage
+submodel_gpkg <- stringr::str_glue("data/c_submodel_data/{submodel}.gpkg")
+
+#### intermediate directories
+output_gpkg <- stringr::str_glue("data/b_intermediate_data/{region}_{data_name}.gpkg")
+
+#####################################
+
+# inspect layers within geodatabases and geopackages
+sf::st_layers(dsn = data_dir,
+              do_count = T)
+
+sf::st_layers(dsn = study_region_gpkg,
+              do_count = T)
+
+#####################################
+#####################################
+
+# load data
+## aids to navigation data (source: https://marinecadastre.gov/downloads/data/mc/AtoN.zip)
+### metadata: https://www.fisheries.noaa.gov/inport/item/56120
+data <- sf::st_read(dsn = data_dir,
+                               # aids to navigation site
+                               layer = sf::st_layers(data_dir)[[1]][grep(pattern = stringr::str_glue("{pattern}"),
+                                                                         x = sf::st_layers(data_dir)[[1]])]) %>%
+  # change to correct coordinate reference system (EPSG:5070 -- NAD83 / CONUS Albers)
+  sf::st_transform(x = .,
+                   crs = crs) %>%
+  # apply 500-meter setback
+  sf::st_buffer(x = .,
+                dist = setback)
+
+## inspect CRS values for the data
+cat(crs(data))
+
+#####################################
+
+## study region
+study_region <- sf::st_read(dsn = study_region_gpkg, layer = stringr::str_glue("{region}_study_region")) %>%
+  # change to correct coordinate reference system (EPSG:5070 -- NAD83 / CONUS Albers)
+  sf::st_transform(crs = crs)
+
+## hex grid
+region_hex <- sf::st_read(dsn = study_region_gpkg, layer = stringr::str_glue("{region}_hex_grid")) %>%
+  # change to correct coordinate reference system (EPSG:5070 -- NAD83 / CONUS Albers)
+  sf::st_transform(crs = crs)
+
+#####################################
+#####################################
+
+# limit data to study region
+region_data <- data %>%
+  # obtain only aids to navigation in the study area
+  rmapshaper::ms_clip(target = .,
+                      clip = study_region) %>%
+  # create field called "layer" and fill with "aids to navigation" for summary
+  dplyr::mutate(layer = stringr::str_glue("{layer_name}"))
+
+#####################################
+#####################################
+
+# aids to navigation hex grids
+region_data_hex <- region_hex[region_data, ] %>%
+  # spatially join aids to navigation values to Gulf of Mexico hex cells
+  sf::st_join(x = .,
+              y = region_data,
+              join = st_intersects) %>%
+  # select fields of importance
+  dplyr::select(GRID_ID, layer) %>%
+  # group by the ID values as there are duplicates
+  dplyr::group_by(GRID_ID) %>%
+  # summarise the grid values
+  dplyr::summarise()
+
+#####################################
+#####################################
+
+# export data
+## constraints geopackage
+sf::st_write(obj = region_data_hex, dsn = submodel_gpkg, layer = stringr::str_glue("{region}_hex_{data_name}"), append = F)
+
+## aids to navigation geopackage
+sf::st_write(obj = data, dsn = output_gpkg, layer = stringr::str_glue("{data_name}"), append = F)
+sf::st_write(obj = region_data, dsn = output_gpkg, layer = stringr::str_glue("{region}_{data_name}"), append = F)
+
+#####################################
+#####################################
+
+# calculate end time and print time difference
+print(Sys.time() - start) # print how long it takes to calculate

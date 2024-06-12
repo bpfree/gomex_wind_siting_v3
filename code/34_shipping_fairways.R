77145dd66d034ba01985a95f4c3f7110974462ca
bpfree <brian.patrick.free@gmail.com>
2024-06-12 15:18:34 -0400

    shipping fairways

---
+++ code/34_shipping_fairways.R
@@ -0,0 +1,150 @@
+#############################
+### 34. shipping fairways ###
+#############################
+
+# clear environment
+rm(list = ls())
+
+# calculate start time of code (determine how long it takes to complete all code)
+start <- Sys.time()
+
+#####################################
+#####################################
+
+# set parameters
+## designate region name
+region <- "gomex"
+
+## designate submodel
+submodel <- "constraints"
+
+## layer names
+data_name <- "shipping_fairways"
+layer_name <- "shipping fairways"
+
+## setback distance (in meters)
+setback <- 3704 # equivalent to 2 nautical miles
+
+## coordinate reference system
+### set the coordinate reference system that data should become (NAD83 / Conus Albers: https://epsg.io/5070)
+crs <- "EPSG:5070"
+
+## designate date
+date <- format(Sys.Date(), "%Y%m%d")
+
+#####################################
+#####################################
+
+# load packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(renv,
+               dplyr,
+               ggplot2,
+               janitor,
+               plyr,
+               purrr,
+               rmapshaper,
+               RSelenium,
+               sf,
+               sp,
+               stringr,
+               targets,
+               terra, # is replacing the raster package
+               tidyr)
+
+#####################################
+#####################################
+
+# set directories
+## define data directory (as this is an R Project, pathnames are simplified)
+### input directories
+#### shipping fairways sites
+data_dir <- "data/a_raw_data/shippinglanes"
+
+#### study area grid
+study_region_gpkg <- stringr::str_glue("data/b_intermediate_data/{region}_study_area.gpkg")
+
+### output directories
+#### submodel geopackage
+submodel_gpkg <- stringr::str_glue("data/c_submodel_data/{submodel}.gpkg")
+
+#### intermediate directories
+output_gpkg <- stringr::str_glue("data/b_intermediate_data/{region}_{data_name}.gpkg")
+
+#####################################
+
+# inspect layers within geopackages
+sf::st_layers(dsn = study_region_gpkg,
+              do_count = T)
+
+#####################################
+#####################################
+
+# load data
+## read shipping fairways (source: http://encdirect.noaa.gov/theme_layers/data/shipping_lanes/shippinglanes.zip)
+### Gulf of Mexico specific: https://www.data.boem.gov/Mapping/Files/Fairways.gdb.zip
+### metadata: https://www.data.boem.gov/Mapping/Files/fairways_meta.html
+data <- sf::st_read(file.path(data_dir, "shippinglanes.shp")) %>%
+  # reproject the coordinate reference system
+  sf::st_transform(crs) %>% # EPSG 5070 (https://epsg.io/5070)
+  # create setback (buffer) of 3704 meters (2 nautical miles)
+  sf::st_buffer(dist = setback)
+
+## inspect CRS values for the data
+cat(crs(data))
+
+#####################################
+
+## study region
+study_region <- sf::st_read(dsn = study_region_gpkg, layer = stringr::str_glue("{region}_study_region")) %>%
+  # change to correct coordinate reference system (EPSG:5070 -- NAD83 / CONUS Albers)
+  sf::st_transform(crs = crs)
+
+## hex grid
+region_hex <- sf::st_read(dsn = study_region_gpkg, layer = stringr::str_glue("{region}_hex_grid")) %>%
+  # change to correct coordinate reference system (EPSG:5070 -- NAD83 / CONUS Albers)
+  sf::st_transform(crs = crs)
+
+#####################################
+#####################################
+
+# limit data to study region
+region_data <- data %>%
+  # obtain only shipping fairways in the study area
+  rmapshaper::ms_clip(target = .,
+                      clip = study_region) %>%
+  # create field called "layer" and fill with "shipping fairways" for summary
+  dplyr::mutate(layer = stringr::str_glue("{layer_name}")) %>%
+  # group by ID values to flatten data
+  dplyr::group_by(layer) %>%
+  # summarise the grid values
+  dplyr::summarise()
+
+#####################################
+#####################################
+
+# shipping fairways hex grids
+region_data_hex <- region_hex[region_data, ] %>%
+  # spatially join shipping fairways values to Gulf of Mexico hex cells
+  sf::st_join(x = .,
+              y = region_data,
+              join = st_intersects) %>%
+  # select fields of importance
+  dplyr::select(GRID_ID, layer)
+
+#####################################
+#####################################
+
+# export data
+## constraints geopackage
+sf::st_write(obj = region_data_hex, dsn = submodel_gpkg, layer = stringr::str_glue("{region}_hex_{data_name}"), append = F)
+
+## Texas reefs geopackage
+sf::st_write(obj = data, dsn = output_gpkg, layer = stringr::str_glue("{data_name}"), apend = F)
+sf::st_write(obj = region_data, dsn = output_gpkg, layer = stringr::str_glue("{region}_{data_name}"), apend = F)
+
+#####################################
+#####################################
+
+# calculate end time and print time difference
+print(Sys.time() - start) # print how long it takes to calculate
